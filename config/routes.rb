0eab8911d0a8a3c5bb62389c4b59794110982ec0
mohamedhelmy10 <mohamedhelmy51288@gmail.com>
2022-11-16 20:27:13 +0200

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'users#index'
   resources :users, only: [:create, :index, :show, :update, :destroy]
   resources :projects, only: [:create, :index, :show, :update, :destroy]
 end
